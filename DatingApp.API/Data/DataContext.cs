f726b05445b8d5f0d439fd4c08af81e895b7d44f
ZachFey <zach.fey@parivedasolutions.com>
2019-12-30 14:41:41 -0500

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.API.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        
+        //Values represents the table name in the database
+        public DbSet<Value> Values { get; set; }
+    }
+}
